26ecb9199acbae769542887e09e013475c383ba8
Krzysztof Sychla <ksychla@antmicro.com>
2022-11-02 14:45:59 +0100

    Add camera tests

---
+++ camera/camera.py
@@ -0,0 +1,14 @@
+import os
+import cv2
+
+class Camera:
+    def __init__(self, number=0, path="/dev/video"):
+        self.number = number
+        self.path = path
+        self.device = cv2.VideoCapture(0)
+        assert self.device.isOpened() "Could not open video device"
+
+    def get_frame(self):
+        ret, frame = cap.read()
+
+
