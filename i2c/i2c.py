521804bd9bc6ff37c62d2b0f5a67f6f4a1152662
Krzysztof Sychla <ksychla@antmicro.com>
2022-11-02 14:45:59 +0100

    Add camera tests

--- i2c/i2c.py
+++ i2c/i2c.py
@@ -1,10 +1,26 @@
+from smbus2 import SMBus
+
 class I2C:
-    def __init__(self, address):
-        self.address = address
+    def __init__(self, bus):
+        self.bus = SMBus(bus)
 
-    def check_connection(self):
-        pass
+    def check_address(self, address, force=False):
+        try:
+            self.bus.read_byte(address, force=force)
+            return True
+        except OSError:
+            return False
 
-    @staticmethod
-    def i2cdetect():
-        pass    # This one is static
+    def i2cdetect(self, force=False):
+        detected_addresses = []
+        for address in range(0x03, 0x78):
+            if self.check_address(address, force=force):
+                detected_addresses.append(address)
+        return detected_addresses
+
+    def read_data(self, address, register=0, force=False):
+        return self.bus.read_byte_data(address, register, force=force)
+
+    def write_data(self, address, value, register=0, force=False):
+        return self.bus.write_byte_data(address, register, value, force=force)
+
