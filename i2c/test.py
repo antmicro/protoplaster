b74b05b95275093ded77ccb37a95383a20f2c0ed
Krzysztof Sychla <ksychla@antmicro.com>
2022-11-09 15:12:49 +0100

    Add pytest wrapper script

--- i2c/test.py
+++ i2c/test.py
@@ -1,8 +1,8 @@
-import i2c
+from i2c.i2c import I2C
 
 class TestI2C:
     def test_addresses(self, bus, addresses):
-        i2c_bus = i2c.I2C(bus)
+        i2c_bus = I2C(bus)
         detected_addresses = i2c_bus.i2cdetect(force=True)
         for addr in addresses:
             assert addr in detected_addresses
