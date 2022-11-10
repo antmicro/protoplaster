6ecc046ca3132168dc0b008b43248e3ccedd4867
Krzysztof Sychla <ksychla@antmicro.com>
2022-11-10 15:21:02 +0100

    Add dynamic fixture loading from yaml

--- conftest.py
+++ conftest.py
@@ -7,9 +7,18 @@ def pytest_addoption(parser):
         "--yaml_file", action="store", type=str, help="Test description yaml"
     )
 
-@pytest.fixture
+@pytest.fixture(scope='class', autouse=True)
+def callattr_ahead_of_alltests(yaml_file, request):
+    arg_name = request.cls.module_name()
+    if arg_name in yaml_file:
+        thing = next(yaml_file[arg_name])
+        for key in thing:
+            setattr(request.cls, key, thing[key])
+
+
+@pytest.fixture(scope='module')
 def yaml_file(request):
     with open(request.config.getoption("--yaml_file")) as file:
         content = yaml.safe_load(file)
-    return content
+    return {k: iter(content[k]) for k in content}
 
