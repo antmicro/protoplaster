50cb847704f54be2a1db4e65b1a1af2637486a4e
Krzysztof Sychla <ksychla@antmicro.com>
2022-11-14 12:48:46 +0100

    Add groups to yaml

--- conftest.py
+++ conftest.py
@@ -4,11 +4,12 @@ import yaml
 
 def pytest_addoption(parser):
     parser.addoption(
-        "--yaml_file", action="store", type=str, help="Test description yaml"
-    )
+        "--yaml_file", action="store", type=str, help="Test description yaml")
+    parser.addoption(
+        "--group", action="store", type=str, help="Group to execute")
 
 @pytest.fixture(scope='class', autouse=True)
-def callattr_ahead_of_alltests(yaml_file, request):
+def setup_tests(yaml_file, request):
     arg_name = request.cls.module_name()
     if arg_name in yaml_file:
         thing = next(yaml_file[arg_name])
@@ -16,9 +17,14 @@ def callattr_ahead_of_alltests(yaml_file, request):
             setattr(request.cls, key, thing[key])
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope='session')
 def yaml_file(request):
     with open(request.config.getoption("--yaml_file")) as file:
         content = yaml.safe_load(file)
+    group = request.config.getoption("--group")
+    if group is not None and group in content.keys():
+        content = content[group]
+    elif group is None:
+        content = {mod_key: content[group_key][mod_key] for group_key in content for mod_key in content[group_key]}
     return {k: iter(content[k]) for k in content}
 
