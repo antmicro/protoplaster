c7bddd1aaad058d13cb992a461f3a877432346a8
Krzysztof Sychla <ksychla@antmicro.com>
2023-01-11 12:50:21 +0100

    [#39214] tools: add user input tools

---
+++ protoplaster/tools/tools.py
@@ -0,0 +1,4 @@
+def assert_user_input(user_message, assertion_message="User input failed", possible_answers=["y", "n"], correct_answer=0):
+	answer = input(f"{user_message} [{'/'.join([ans.upper() if i == correct_answer else ans for i, ans in enumerate(possible_answers)])}] ")
+	if answer != "":
+		assert answer.lower() == possible_answers[correct_answer].lower()
