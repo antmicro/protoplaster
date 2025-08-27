0c34dfd88249c0dc7e38f067e31f98ddadcbdf62
Maciej Sobkowski <msobkowski@antmicro.com>
2025-08-27 10:27:54 +0200

    [#82348] Add API docs
    Signed-off-by: Maciej Sobkowski <msobkowski@antmicro.com>

--- .github/scripts/latex.sh
+++ .github/scripts/latex.sh
@@ -2,12 +2,16 @@
 
 set -e
 
+apt-get update -qq
+DEBIAN_FRONTEND=noninteractive apt-get -y install --no-install-recommends git
+
 cd $(dirname $0)/../../docs
 
 python3 -m venv ./venv
 . ./venv/bin/activate
 
 pip3 install -r requirements.txt
+pip3 install ../
 
 cd build/latex
 LATEXMKOPTS='-interaction=nonstopmode' make
