9a35744c6b0f6fc80e6e73273098f9e571f122b5
Dawid Olszewski <dolszewski@antmicro.com>
2026-02-04 13:07:09 +0100

    [#90452] TCA9648A: Add test for mask reset via RESET

--- protoplaster/tests/i2c_mux/TCA9548A/TCA9548A.py
+++ protoplaster/tests/i2c_mux/TCA9548A/TCA9548A.py
@@ -1,3 +1,5 @@
+import time
+from protoplaster.tests.gpio.gpio.gpio import GPIO, Direction
 from smbus2 import SMBus
 from protoplaster.tests.i2c_mux.I2CMux import I2CMux
 
@@ -7,9 +9,11 @@ class TCA9548A(I2CMux):
     def __init__(self,
                  i2c_bus: int,
                  i2c_address: int,
-                 smbus_force: bool = False):
+                 smbus_force: bool = False,
+                 reset_gpio_number: int = None):
         self.bus = SMBus(i2c_bus, force=smbus_force)
         self.address = i2c_address
+        self.rst_gpio = reset_gpio_number
         self.fail_reason = None
 
     def is_alive(self):
@@ -24,6 +28,19 @@ class TCA9548A(I2CMux):
         except Exception as e:
             self.fail_reason = f"Error communicating with device: {e}"
         return False
+
+    def reset(self):
+        if self.rst_gpio is None:
+            return False
+        try:
+            with GPIO(self.rst_gpio, Direction.OUT) as rst_gpio:
+                rst_gpio.write_value(0)
+                time.sleep(1)  # at least 6 ns
+                rst_gpio.write_value(1)
+        except Exception as e:
+            self.fail_reason = f"Error accessing rst_gpio {self.rst_gpio}: {e}"
+            return False
+        return True
 
     def get_mask(self) -> int:
         return self.bus.read_byte(self.address)
--- protoplaster/tests/i2c_mux/TCA9548A/test.py
+++ protoplaster/tests/i2c_mux/TCA9548A/test.py
@@ -1,5 +1,6 @@
 from protoplaster.conf.module import ModuleName
 from protoplaster.tests.i2c_mux.TCA9548A.TCA9548A import TCA9548A
+import pytest
 
 
 @ModuleName("TCA9548A")
@@ -12,16 +13,38 @@ class TestTCA9548A:
     {%- endmacro %}
     """
 
+    mask_after_reset = 0x0
+
+    def setup_class(self):
+        self.smbus_force = getattr(self, "smbus_force", False)
+        self.reset_gpio = getattr(self, "reset_gpio", None)
+        self.mux = TCA9548A(self.i2c_bus,
+                            self.i2c_address,
+                            smbus_force=self.smbus_force,
+                            reset_gpio=self.reset_gpio)
+
     def test_is_alive(self):
         """
         {% macro test_sysfs_interface(device) -%}
         check if TCA9548A responds correctly to simple requests
         {%- endmacro %}
         """
-        self.smbus_force = getattr(self, "smbus_force", False)
-        mux = TCA9548A(self.i2c_bus, self.i2c_address, self.smbus_force)
-        assert mux.is_alive(
-        ), f"TCA9548A does not respond correctly. Reason: {mux.fail_reason}"
+        assert self.mux.is_alive(
+        ), f"TCA9548A does not respond correctly. Reason: {self.mux.fail_reason}"
+
+    def test_reset(self):
+        """
+        {% macro test_reset(device) -%}
+        Check whether the TCA9548A responds correctly to a reset triggered by pulling {{ device['reset_gpio'] }} low
+        {%- endmacro %}
+        """
+        if self.reset_gpio is None:
+            pytest.skip("'reset_gpio' parameter not set")
+        arbitrary_mask = 0xA1
+        self.mux.set_mask(arbitrary_mask)
+        self.mux.reset()
+        current_mask = self.mux.get_mask()
+        assert current_mask == TestTCA9548A.mask_after_reset, f"Reset mask mismatch: expected {hex(TestTCA9548A.mask_after_reset)}, read {hex(current_mask)}"
 
     def name(self):
         return f"TCA9548A({self.i2c_bus}, {hex(self.i2c_address)})"
