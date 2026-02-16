0ca9376b94849bea67a5e84f277f162b05daa55b
Dawid Olszewski <dolszewski@antmicro.com>
2026-02-16 18:29:40 +0100

    [#91314] module.py: Get rid of variadic arguments

--- protoplaster/conf/module.py
+++ protoplaster/conf/module.py
@@ -11,15 +11,15 @@ class ModuleName(object):
     def __init__(self, name):
         self.module_name = name
 
-    def __call__(self, *param_arg):
+    def __call__(self, test_class):
         # Build the members dict manually using reversed MRO and __dict__.
         # We avoid inspect.getmembers() because it sorts alphabetically,
         # and we want to preserve the exact order the tests were defined in the file.
         # First, all test_metods of the derived class are executed, and then the methods of the base class
         members = {}
-        for base_class in param_arg[0].__mro__:
+        for base_class in test_class.__mro__:
             for name in base_class.__dict__:
-                members.setdefault(name, getattr(param_arg[0], name))
+                members.setdefault(name, getattr(test_class, name))
 
         for name, member in members.items():
             if (name.startswith("test") and
@@ -27,12 +27,12 @@ class ModuleName(object):
                 # setting attribute does not change order in __dict __ if it is already set
                 # to achive correct order we first have to delete it, and then set it again
                 try:
-                    delattr(param_arg[0], name)
+                    delattr(test_class, name)
                 except AttributeError:
                     pass
-                setattr(param_arg[0], name, member)
+                setattr(test_class, name, member)
 
-        name_method = getattr(param_arg[0], "name", None)
+        name_method = getattr(test_class, "name", None)
         if not callable(name_method):
             raise TypeError("Class is missing name() method")
 
@@ -40,5 +40,5 @@ class ModuleName(object):
         def module_name():
             return self.module_name
 
-        setattr(param_arg[0], "module_name", module_name)
-        return param_arg[0]
+        setattr(test_class, "module_name", module_name)
+        return test_class
