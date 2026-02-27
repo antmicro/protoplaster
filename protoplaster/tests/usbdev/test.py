0a0c7d3a3a29133334b46b9ded90bf7e8c439418
Patryk Koscik <pkoscik@antmicro.com>
2026-02-27 12:53:36 +0100

    [#92350] tests: usb: add `test_negotiated_speed` test

--- protoplaster/tests/usbdev/test.py
+++ protoplaster/tests/usbdev/test.py
@@ -1,5 +1,6 @@
 from protoplaster.conf.module import ModuleName
 import os
+import pytest
 
 
 @ModuleName("usbdev")
@@ -17,6 +18,27 @@ class TestUsb:
         """
         assert os.path.exists(
             self.sysfs_path), f"USB interface {self.sysfs_path} doesn't exist"
+
+    def test_negotiated_speed(self):
+        """
+        {% macro test_negotiated_speed(device) -%}
+          check if the negotiated USB speed is {{ device['speed'] }} Mbps
+        {%- endmacro %}
+        """
+        if not hasattr(self, "speed"):
+            pytest.skip("speed parameter not set")
+
+        speed_file = os.path.join(self.sysfs_path, "speed")
+
+        assert os.path.exists(
+            speed_file
+        ), f"Could not find speed file: {speed_file} (is the device connected?)"
+
+        with open(speed_file, "r") as f:
+            current_speed = f.read().strip()
+
+        assert current_speed == str(self.speed), \
+            f"Expected USB speed {self.speed} Mbps, but got {current_speed} Mbps"
 
     def name(self):
         return f"usb({self.device})"
